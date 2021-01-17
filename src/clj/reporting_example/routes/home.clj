9445ff79d3288a415daa54af4bf29f29e79e4ba8
cindy <ciindy.wu@gmail.com>
2021-01-17 10:46:14 -1000

    Adds button to turn employee data into PDF

--- src/clj/reporting_example/routes/home.clj
+++ src/clj/reporting_example/routes/home.clj
@@ -1,17 +1,35 @@
 (ns reporting-example.routes.home
   (:require [reporting-example.layout :as layout]
+            [reporting-example.reports :as reports]
             [compojure.core :refer [defroutes GET]]
-            [ring.util.http-response :as response]
+            [ring.util.response :as response]
             [clojure.java.io :as io]))
 
 (defn home-page []
-  (layout/render
-    "home.html" {:docs (-> "docs/docs.md" io/resource slurp)}))
+  (layout/render "home.html"))
+
+(defn write-response [report-bytes]
+  (with-open [in (java.io.ByteArrayInputStream. report-bytes)]
+    (-> (response/response in)
+        (response/header "Content-Disposition" "filename=document.pdf")
+        (response/header "Content-Length" (count report-bytes))
+        (response/content-type "application/pdf"))))
+
+(defn generate-report [report-type]
+  (try
+    (let [out (java.io.ByteArrayOutputStream.)]
+      (condp = (keyword report-type)
+        :table (reports/table-report out)
+        :list (reports/list-report out))
+      (write-response (.toByteArray out)))
+    (catch Exception ex
+      (layout/render "home.html" {:error (.getMessage ex)}))))
 
 (defn about-page []
   (layout/render "about.html"))
 
 (defroutes home-routes
   (GET "/" [] (home-page))
-  (GET "/about" [] (about-page)))
+  (GET "/about" [] (about-page))
+  (GET "/:report-type" [report-type] (generate-report report-type)))
 
